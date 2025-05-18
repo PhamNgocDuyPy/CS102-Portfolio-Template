fbe1195111d3d62c5925796cf1a4b5dab206ac17
DUY PHẠM NGỌC <23122025@student.hcmus.edu.vn>
2025-05-18 16:30:27 +0700

    ngon

--- app/data.ts
+++ app/data.ts
@@ -55,7 +55,7 @@ export const PROJECTS: Project[] = [
     description: 'UI kit to make beautiful, animated interfaces.',
     link: 'https://motion-primitives.com/',
     video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4',
+      'https://res.cloudinary.com/dz1dhavd2/image/upload/v1747560582/hot-girl-pttm-20-1615657390286_n7mz5q.webp',
     id: 'project2',
   },
 ]
